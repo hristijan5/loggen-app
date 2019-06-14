615858765de1b84e274bc44f48e7bbdb166778f5
Hristijan Petreski <hristijan.petreski@softcom-int.com>
2019-06-14 13:16:59 +0200

    Initial commit

---
+++ src/main/java/com/softcomint/propml/PropmlLoggingToolApplication.java
@@ -0,0 +1,13 @@
+package com.softcomint.propml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropmlLoggingToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PropmlLoggingToolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/softcomint/propml/PropmlLoggingToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softcomint.propml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropmlLoggingToolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
